7f67d54eccf87d8c5b475a65904b75cb2f279142
ryramzy <mramsay0@gmail.com>
2025-04-07 23:42:44 -0300

    fix: update border color configuration in Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,6 +53,9 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      borderColor: {
+        DEFAULT: "hsl(var(--border))",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
