ac0aed3417832acea3ffba69189e819c723e67f3
ryramzy <mramsay0@gmail.com>
2025-04-07 23:27:56 -0300

    chore: update project configuration for Vercel deployment - Remove yarn.lock - Add vercel.json - Update package.json for npm - Add .nvmrc for Node.js version

--- next.config.js
+++ next.config.js
@@ -5,45 +5,12 @@ const nextConfig = {
     domains: ['techmissionrio.org'],
     formats: ['image/avif', 'image/webp'],
   },
-  experimental: {
-    optimizePackageImports: ['@radix-ui/react-icons'],
-  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on',
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-        ],
-      },
-    ]
-  },
+  swcMinify: true,
+  poweredByHeader: false,
+  compress: true,
 }
 
 module.exports = nextConfig 
